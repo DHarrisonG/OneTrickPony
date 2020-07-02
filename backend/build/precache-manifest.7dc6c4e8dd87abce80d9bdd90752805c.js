ea4ba86477dadc7608d97baa6e7e97ed08417995
github username <github email address>
2020-07-02 09:55:01 -0500

    initializing backend

---
+++ backend/build/precache-manifest.7dc6c4e8dd87abce80d9bdd90752805c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5d74d4136c4e06e5643aa0b4f637dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32eec1226e85305e9480",
+    "url": "./static/css/main.e8ff79f5.chunk.css"
+  },
+  {
+    "revision": "624740a8fcde4f6aee00",
+    "url": "./static/js/2.7135167b.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.7135167b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32eec1226e85305e9480",
+    "url": "./static/js/main.acfe1639.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
